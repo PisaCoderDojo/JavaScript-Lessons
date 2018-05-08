2146b77bcceb4200c8f063c313cbdd8b8148b1e5
Salvatore Ventrone <ventrosky@gmail.com>
2018-05-08 20:52:28 +0200

    Update progetto2generatore.js

--- progetto2generatore.js
+++ progetto2generatore.js
@@ -1,4 +1,4 @@
-// Progettino n°2
+// Progettino n°2 Generatore di Insulti
 
 var partiDelCorpo = ["Volto", "Naso", "Taglio di Capelli", "Mento"]; 
 var aggettivi = ["Puzzolente", "Noiosa", "Sciocca"]; 
@@ -10,3 +10,10 @@ var randomInsulto = "Il tuo " + randomCorpo + " è come una " + randomAggettivo 
 randomInsult; //= "Il tuo Naso è come una Sciocca Marmotta!!!"
 //var randomParola = parole[Math.floor(Math.random() * parole.length)];
 //var randomInsulto = ["Il tuo", randomCorpo, "è", "come", "una", randomAggettivo, randomParola + "!!!"].join(" ");
+
+/* 
+Challenges :
+-1) nuove parole; 
+-2) più sofisticati (“Il tuo [parte del corpo] è più [aggettivo] di un [parte del corpo di animale] di [animale] .”; 
+-3) usa join; 
+*/
